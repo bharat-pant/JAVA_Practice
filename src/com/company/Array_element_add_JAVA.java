39cd869992528871a7c7c9a0120e9f2b024c8d4b
bharat-pant <bpant268@gmail.com>
2021-06-19 23:58:00 +0530

    Day 8: JAVA Programming-> Miscelaneous Problems on function and arrays

---
+++ src/com/company/Array_element_add_JAVA.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+
+public class Array_element_add_JAVA {
+    int [] element_array = new int[10];
+    static void add_element(int i)
+    {
+        Scanner scan_values = new Scanner(System.in);
+        int input_value = scan_values.nextInt();
+        element_array[i] = input_value;
+        System.out.println(element_array[i]);
+        for (int j=0; j<5; j++ )
+        {
+            System.out.print(element_array[j]+ " ");;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan_values = new Scanner(System.in);
+        int no_of_elements = scan_values.nextInt();
+        for (int i=0; i<no_of_elements; i++ )
+        {
+            add_element(i);
+        }
+
+    }
+}
